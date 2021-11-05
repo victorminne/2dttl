27661ad0a76cb263e1b7ffd7eb1c3c47073fe909
Victor Minne <3aminne.v@gmail.com>
2021-11-05 13:47:22 +0100

    data and html
    add the data import and the html

--- projet.py
+++ projet.py
@@ -2,3 +2,17 @@ import pandas as pd
 import numpy as np
 import seaborn as sb 
 import matplotlib as mp 
+import os
+import webbrowser
+
+
+#import data as dataframe
+data = pd.read_csv("weather_madrid.csv")
+
+
+#html part 
+html = data[0:50].to_html()
+with open("data.html", "w") as f:
+    f.write(html)
+full_filename = os.path.abspath("data.html")
+webbrowser.open(f"file://{full_filename}")
